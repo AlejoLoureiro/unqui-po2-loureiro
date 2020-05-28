2ce65e534013bdd60ce450dd94731b475d85c9d0
Alejo Loureiro <loureiroalejo@gmail.com>
2020-05-28 18:30:44 -0300

    tp template y adapter terminado

---
+++ src/ar/edu/unq/po2/tpTemplateAdapter/cuentaBancaria/CuentaCorriente.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.po2.tpTemplateAdapter.cuentaBancaria;
+
+public class CuentaCorriente extends CuentaBancaria {
+
+	private int descubierto;
+	
+	public CuentaCorriente(String titular, int descubierto){
+		super(titular);
+		this.descubierto=descubierto;
+	}
+	
+	public int getDescubierto(){
+		return this.descubierto;
+	}
+	
+	@Override
+	public boolean puedoExtraer(int monto) {
+		return (this.getSaldo()+this.getDescubierto()>=monto);
+	}
+
+}
