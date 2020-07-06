a2946231fb6f112f8f1af7bf7465760c07c6b7a4
Alejo Loureiro <loureiroalejo@gmail.com>
2020-07-06 02:06:06 -0300

    tp test

---
+++ src/ar/edu/unq/po2/tpTest/pokerStatus/PokerStatus.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.po2.tpTest.pokerStatus;
+
+public class PokerStatus {
+
+	public boolean verificar(String c1, String c2, String c3, String c4, String c5) {
+		return false;
+	}
+}
